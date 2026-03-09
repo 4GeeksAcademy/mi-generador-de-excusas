e5c2d89a2daa7dedeb5e3f6c7b2194a35b7b519c
Juan Donday Rodríguez <jdondayr@gmail.com>
2026-03-09 12:49:03 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    function generadorAleatorioExcusas() {
+    let who = ["El perro", "Mi profesor", "Mi padre", "Un hipopótamo"]; let randomWho = Math.floor(Math.random() * who.length);
+    let action = ["se ha comido", "ha dormido sobre", "ha destrozado"]; let randomAction = Math.floor(Math.random() * action.length);
+    let what = ["mis deberes", "mi coche", "mi cama"]; let randomWhat = Math.floor(Math.random() * what.length);
+    let when = ["esta noche", "ahora mismo", "hace dos días"]; let randomWhen = Math.floor(Math.random() * when.length);
+    
+    return (`${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`)
+  };
+  
+  document.querySelector("#excuse").innerHTML = generadorAleatorioExcusas();
 };
